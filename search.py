afd3e9a1d6c827a8023451efc97a33eacf0de7ba
shirin <shirinedl@gmail.com>
2017-05-07 09:57:56 +0530

    Adds zernike moments to search

--- search.py
+++ search.py
@@ -3,44 +3,44 @@ import searcher
 import argparse
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--index", required = True,
-	help = "Path to where the computed index will be stored")
-ap.add_argument("-q", "--query", required = True,
-	help = "Path to the query image")
-ap.add_argument("-r", "--result-path", required = True,
-	help = "Path to the result path")
-args = vars(ap.parse_args())
+def main(query, index, result, main=False):
+    #load the query image
+    query = cv2.imread(query)
 
-cd = colordescriptor.ColorDescriptor((8, 12, 3))
+    #perform the search
+    s = searcher.Searcher(index)
+    results = s.search(query)
 
-#load the query image and describe it
-query = cv2.imread(args["query"])
-features = cd.describe(query)
+    """for (result, idd) in results:
+        cv2.imshow("Result", result)
+        cv2.waitKey(0)"""
 
-#perform the search
-s = searcher.Searcher(args["index"])
-results = s.search(features)
-
-"""for (result, idd) in results:
-    cv2.imshow("Result", result)
-    cv2.waitKey(0)"""
-
-#display the query
-cv2.namedWindow('Query', cv2.WINDOW_NORMAL)
-cv2.imshow("Query", query)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#loop over the results
-
-cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-
-for (score, imgpath) in results:
-    #print "ResultID ", resultID
-    #result = cv2.imread(args["result_path"] + "/" + resultID)
-    img = cv2.imread(imgpath)
-    cv2.imshow("Result", img)
+    #display the query
+    cv2.namedWindow('Query', cv2.WINDOW_NORMAL)
+    cv2.imshow("Query", query)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.destroyAllWindows()
+    #loop over the results
+
+    cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
+
+    for imgpath in results:
+        #print "ResultID ", resultID
+        #result = cv2.imread(args["result_path"] + "/" + resultID)
+        img = cv2.imread(imgpath)
+        cv2.imshow("Result", img)
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--index", required = True,
+                    help = "Path to where the computed index will be stored")
+    ap.add_argument("-q", "--query", required = True,
+                    help = "Path to the query image")
+    ap.add_argument("-r", "--result-path", required = True,
+                    help = "Path to the result path")
+    args = vars(ap.parse_args())
+    main(args['query'], args['index'], args['result_path'], main=True)
--- searcher.py
+++ searcher.py
@@ -1,5 +1,11 @@
 import numpy as np
 import csv
+import os
+
+from scipy.spatial import distance as dist
+
+import colordescriptor
+import zernikemoments
 
 class Searcher:
     def __init__(self, indexPath):
@@ -10,11 +16,14 @@ class Searcher:
                     for (a, b) in zip(histA, histB)])
         return d
 
-    def search(self, queryFeatures, limit = 5):
+    def cdsearch(self, image, limit = 5):
+        #describe the image
+        cd = colordescriptor.ColorDescriptor((8, 12, 3))
+        queryfeatures = cd.describe(image)
         #initialize the dictionary of results
         results = {}
         #open the index file for reading
-        with open(self.indexPath) as f:
+        with open(os.path.join(self.indexPath, 'colordescriptor.csv')) as f:
             #initialize the CSV reader
             reader = csv.reader(f)
 
@@ -23,17 +32,50 @@ class Searcher:
                 #chi-squared distance between the features in our index
                 #and our query features
                 features = [float(x) for x in row[1:]]
-                d = self.chi2_distance(features, queryFeatures)
+                d = self.chi2_distance(features, queryfeatures)
 
                 results[row[0]] = d
-
-
-            f.close()
 
             #sort out the results so that the smaller distances are at
             #the fron of the list
             results = sorted([(v,k) for (k, v) in results.items()])
             
-
             return results[:limit]
 
+    def zmsearch(self, image, limit=5):
+        #describe the query image
+        zm = zernikemoments.ZernikeMoments(25)
+        queryfeatures = zm.describe(image)
+
+        #initialize the dictionary of results
+        results = {}
+        #open the index file for reading
+        with open(os.path.join(self.indexPath, 'zernikemoment.csv')) as f:
+            #initialize the CSV reader
+            reader = csv.reader(f)
+
+            # loop over the images in our index
+            for row in reader:
+                # compute the distance between the query features
+                # and features in our index, then update the results
+                features = [float(x) for x in row[1:]]
+                d = dist.euclidean(queryfeatures, features)
+                results[row[0]] = d
+
+            # sort our results, where a smaller distance indicates
+            # higher similarity
+            results = sorted([(v, k) for (k, v) in results.items()])[:limit]
+
+            # return the results
+            return results
+
+    def search(self, image):
+        cdresult = self.cdsearch(image)
+        cdresult = [imagepath for (score, imagepath) in cdresult]
+        cdresult = set(cdresult)
+        zmresult = self.zmsearch(image)
+        zmresult = [imagepath for (score, imagepath) in zmresult]
+        zmresult = set(zmresult)
+        result = cdresult.union(zmresult)
+
+        return result
